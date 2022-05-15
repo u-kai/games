8bbad220b53b443e1175c24cf0b82d667f8a3dea
u-kai <76635578+u-kai@users.noreply.github.com>
2022-05-15 11:28:18 +0900

    clear get_up

--- src/masu/masu.rs
+++ src/masu/masu.rs
@@ -35,6 +35,20 @@ where
     pub fn is_edge(&self, holizon: usize, valtical: usize) -> bool {
         valtical == (self.0.len() - 1) || holizon == (self.0[0].len() - 1)
     }
+    pub fn is_out_bound(&self, holizon: usize, valtical: usize) -> bool {
+        valtical >= self.0.len() || holizon >= self.0[0].len()
+    }
+    pub fn get_up(&self, holizon: usize, valtical: usize) -> Result<T, String> {
+        if valtical == 0 {
+            return Err(format!("[{},{}] up is out bound", holizon, valtical));
+        }
+        let new_h = holizon;
+        let new_v = valtical - 1;
+        if self.is_out_bound(new_h, new_v) {
+            return Err(format!("[{},{}] up is out bound", holizon, valtical));
+        }
+        Ok(self.get(new_h, new_v))
+    }
 }
 
 #[cfg(test)]
@@ -42,6 +56,24 @@ mod masu_test {
     use crate::osero::stone::OseroStone;
 
     use super::Masu;
+    #[test]
+    fn get_up_test() {
+        let mut masu: Masu<OseroStone> = Masu::new(8, 8);
+        masu.change(2, 2, OseroStone::Black);
+        masu.change(1, 1, OseroStone::White);
+        assert_eq!(masu.get_up(2, 3).unwrap(), OseroStone::Black);
+        assert_eq!(masu.get_up(1, 2).unwrap(), OseroStone::White);
+        assert_eq!(masu.get_up(2, 1).unwrap(), OseroStone::Empty);
+        assert_eq!(masu.get_up(0, 0), Err("[0,0] up is out bound".to_string()));
+        assert_eq!(masu.get_up(7, 0), Err("[7,0] up is out bound".to_string()));
+    }
+    #[test]
+    fn is_out_bound_test() {
+        let masu: Masu<OseroStone> = Masu::new(8, 8);
+        assert_eq!(masu.is_out_bound(8, 0), true);
+        assert_eq!(masu.is_out_bound(1, 9), true);
+        assert_eq!(masu.is_out_bound(7, 7), false);
+    }
     #[test]
     fn is_edge_test() {
         let masu: Masu<OseroStone> = Masu::new(8, 8);
