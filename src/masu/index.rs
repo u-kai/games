720747afddb4c8e5b20a4a3a136d86d46bfe59df
u-kai <76635578+u-kai@users.noreply.github.com>
2022-05-15 12:44:51 +0900

    clear get all dist

--- src/masu/index.rs
+++ src/masu/index.rs
@@ -27,53 +27,69 @@ impl MasuIndex {
     pub fn get_h(&self) -> usize {
         self.h_index
     }
-    pub fn get_down(&self) -> Result<MasuIndex, String> {
-        if self.is_down_edge() {
-            return Err(format!(
-                "[{},{}] down is out bound",
-                self.h_index, self.v_index
-            ));
-        }
-        let new_h = self.h_index;
-        let new_v = self.v_index + 1;
-        let index = self.create(new_h, new_v);
-        Ok(index)
-    }
     pub fn get_up(&self) -> Result<MasuIndex, String> {
         if self.is_up_edge() {
-            return Err(format!(
-                "[{},{}] up is out bound",
-                self.h_index, self.v_index
-            ));
+            return Err(self.err_msg("up"));
         }
-        let new_h = self.h_index;
-        let new_v = self.v_index - 1;
-        let index = self.create(new_h, new_v);
+        let index = self.create(self.h_index, self.v_index - 1);
+        Ok(index)
+    }
+    pub fn get_down(&self) -> Result<MasuIndex, String> {
+        if self.is_down_edge() {
+            return Err(self.err_msg("down"));
+        }
+        let index = self.create(self.h_index, self.v_index + 1);
         Ok(index)
     }
     pub fn get_left(&self) -> Result<MasuIndex, String> {
         if self.is_left_edge() {
-            return Err(format!(
-                "[{},{}] left is out bound",
-                self.h_index, self.v_index
-            ));
+            return Err(self.err_msg("left"));
         }
-        let new_h = self.h_index - 1;
-        let new_v = self.v_index;
-        let index = self.create(new_h, new_v);
+        let index = self.create(self.h_index - 1, self.v_index);
         Ok(index)
     }
     pub fn get_right(&self) -> Result<MasuIndex, String> {
         if self.is_right_edge() {
-            return Err(format!(
-                "[{},{}] right is out bound",
-                self.h_index, self.v_index
-            ));
+            return Err(self.err_msg("right"));
         }
-        let new_h = self.h_index + 1;
-        let new_v = self.v_index;
-        let index = self.create(new_h, new_v);
+        let index = self.create(self.h_index + 1, self.v_index);
         Ok(index)
+    }
+    pub fn get_up_right(&self) -> Result<MasuIndex, String> {
+        match self.get_up() {
+            Ok(index) => {
+                let right_index = index.get_right();
+                right_index
+            }
+            Err(e) => Err(e),
+        }
+    }
+    pub fn get_up_left(&self) -> Result<MasuIndex, String> {
+        match self.get_up() {
+            Ok(index) => {
+                let left_index = index.get_left();
+                left_index
+            }
+            Err(e) => Err(e),
+        }
+    }
+    pub fn get_down_right(&self) -> Result<MasuIndex, String> {
+        match self.get_down() {
+            Ok(index) => {
+                let right_index = index.get_right();
+                right_index
+            }
+            Err(e) => Err(e),
+        }
+    }
+    pub fn get_down_left(&self) -> Result<MasuIndex, String> {
+        match self.get_down() {
+            Ok(index) => {
+                let left_index = index.get_left();
+                left_index
+            }
+            Err(e) => Err(e),
+        }
     }
     fn is_down_edge(&self) -> bool {
         self.v_index == (self.v_len - 1)
@@ -87,11 +103,106 @@ impl MasuIndex {
     fn is_right_edge(&self) -> bool {
         self.h_index == (self.v_len - 1)
     }
+    fn err_msg(&self, posi: &str) -> String {
+        format!("[{},{}] {} is out bound", self.h_index, self.v_index, posi)
+    }
 }
 
 #[cfg(test)]
 mod index_test {
     use crate::masu::index::MasuIndex;
+    #[test]
+    fn get_down_left_test() {
+        assert_eq!(
+            MasuIndex::new(8, 8, 2, 1).get_down_left().unwrap(),
+            MasuIndex::new(8, 8, 1, 2)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 3, 2).get_down_left().unwrap(),
+            MasuIndex::new(8, 8, 2, 3)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 6, 4).get_down_left().unwrap(),
+            MasuIndex::new(8, 8, 5, 5)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 0, 6).get_down_left(),
+            Err("[0,7] left is out bound".to_string())
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 1, 7).get_down_left(),
+            Err("[1,7] down is out bound".to_string())
+        );
+    }
+    #[test]
+    fn get_down_right_test() {
+        assert_eq!(
+            MasuIndex::new(8, 8, 2, 1).get_down_right().unwrap(),
+            MasuIndex::new(8, 8, 3, 2)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 3, 2).get_down_right().unwrap(),
+            MasuIndex::new(8, 8, 4, 3)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 6, 4).get_down_right().unwrap(),
+            MasuIndex::new(8, 8, 7, 5)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 7, 5).get_down_right(),
+            Err("[7,6] right is out bound".to_string())
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 6, 7).get_down_right(),
+            Err("[6,7] down is out bound".to_string())
+        );
+    }
+    #[test]
+    fn get_up_left_test() {
+        assert_eq!(
+            MasuIndex::new(8, 8, 2, 1).get_up_left().unwrap(),
+            MasuIndex::new(8, 8, 1, 0)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 3, 2).get_up_left().unwrap(),
+            MasuIndex::new(8, 8, 2, 1)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 6, 4).get_up_left().unwrap(),
+            MasuIndex::new(8, 8, 5, 3)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 0, 6).get_up_left(),
+            Err("[0,5] left is out bound".to_string())
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 1, 0).get_up_left(),
+            Err("[1,0] up is out bound".to_string())
+        );
+    }
+    #[test]
+    fn get_up_right_test() {
+        assert_eq!(
+            MasuIndex::new(8, 8, 2, 1).get_up_right().unwrap(),
+            MasuIndex::new(8, 8, 3, 0)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 3, 2).get_up_right().unwrap(),
+            MasuIndex::new(8, 8, 4, 1)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 6, 4).get_up_right().unwrap(),
+            MasuIndex::new(8, 8, 7, 3)
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 7, 7).get_up_right(),
+            Err("[7,6] right is out bound".to_string())
+        );
+        assert_eq!(
+            MasuIndex::new(8, 8, 6, 0).get_up_right(),
+            Err("[6,0] up is out bound".to_string())
+        );
+    }
     #[test]
     fn get_right_test() {
         assert_eq!(
