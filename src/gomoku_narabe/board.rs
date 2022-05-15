940f76f7b228753fc5206bcd586ea7fcabe6e6ec
u-kai <76635578+u-kai@users.noreply.github.com>
2022-05-15 22:29:30 +0900

    clear gomoku-board

--- src/gomoku_narabe/board.rs
+++ src/gomoku_narabe/board.rs
@@ -14,27 +14,98 @@ impl GomokuBoard {
     pub fn masu(&self, holizon: usize, valtical: usize) -> CorX {
         self.masu.get(holizon, valtical)
     }
-    pub fn put(&mut self, holizon: usize, valtical: usize, koma: CorX) {
-        self.masu.change(holizon, valtical, koma)
+    pub fn print(&self) {
+        self.masu.print()
+    }
+    pub fn put(&mut self, holizon: usize, valtical: usize, koma: CorX) -> Result<(), String> {
+        if self.is_puttable(holizon, valtical) {
+            self.masu.change(holizon, valtical, koma);
+            return Ok(());
+        }
+        Err(format!("can not put [{},{}]", holizon, valtical))
     }
     pub fn is_puttable(&self, holizon: usize, valtical: usize) -> bool {
         self.masu(holizon, valtical) == CorX::Empty
     }
     pub fn winner(&self) -> Option<CorX> {
         // check indexs
-        //0,0 down right down-right
-
-        if self.is_down_aligne(0, 0) {
+        if self.is_down_right_align(0, 0) {
             return Some(self.masu(0, 0));
         }
-        //1,0 down
-        //2,0 down
-        //0,1 right
-        //0,2 right
+        if self.is_down_left_align(2, 0) {
+            return Some(self.masu(2, 0));
+        }
+        for h in 0..3 {
+            if self.is_down_align(h, 0) {
+                return Some(self.masu(h, 0));
+            }
+        }
+        for v in 0..3 {
+            if self.is_right_align(v, 0) {
+                return Some(self.masu(v, 0));
+            }
+        }
         //todo
         None
     }
-    fn is_down_aligne(&self, holizon: usize, valtical: usize) -> bool {
+    fn is_down_left_align(&self, holizon: usize, valtical: usize) -> bool {
+        let koma = self.masu(holizon, valtical);
+        match koma {
+            CorX::Empty => false,
+            _ => {
+                let down = self.masu.get_down_left(holizon, valtical);
+                match down {
+                    Ok(down) => {
+                        if down == koma {
+                            self.is_down_left_align(holizon - 1, valtical + 1)
+                        } else {
+                            false
+                        }
+                    }
+                    Err(_) => true,
+                }
+            }
+        }
+    }
+    fn is_down_right_align(&self, holizon: usize, valtical: usize) -> bool {
+        let koma = self.masu(holizon, valtical);
+        match koma {
+            CorX::Empty => false,
+            _ => {
+                let down = self.masu.get_down_right(holizon, valtical);
+                match down {
+                    Ok(down) => {
+                        if down == koma {
+                            self.is_down_right_align(holizon + 1, valtical + 1)
+                        } else {
+                            false
+                        }
+                    }
+                    Err(_) => true,
+                }
+            }
+        }
+    }
+    fn is_right_align(&self, holizon: usize, valtical: usize) -> bool {
+        let koma = self.masu(holizon, valtical);
+        match koma {
+            CorX::Empty => false,
+            _ => {
+                let down = self.masu.get_right(holizon, valtical);
+                match down {
+                    Ok(down) => {
+                        if down == koma {
+                            self.is_right_align(holizon + 1, valtical)
+                        } else {
+                            false
+                        }
+                    }
+                    Err(_) => true,
+                }
+            }
+        }
+    }
+    fn is_down_align(&self, holizon: usize, valtical: usize) -> bool {
         let koma = self.masu(holizon, valtical);
         match koma {
             CorX::Empty => false,
@@ -43,7 +114,7 @@ impl GomokuBoard {
                 match down {
                     Ok(down) => {
                         if down == koma {
-                            self.is_down_aligne(holizon, valtical + 1)
+                            self.is_down_align(holizon, valtical + 1)
                         } else {
                             false
                         }
@@ -67,10 +138,21 @@ mod gomoku_test {
         board.put(0, 1, CorX::Circle);
         board.put(0, 2, CorX::Circle);
         assert_eq!(board.winner(), Some(CorX::Circle));
+        let mut board = GomokuBoard::new();
+        board.put(0, 0, CorX::Circle);
+        board.put(1, 0, CorX::Circle);
+        board.put(2, 0, CorX::Circle);
+        assert_eq!(board.winner(), Some(CorX::Circle));
+        let mut board = GomokuBoard::new();
+        board.put(0, 0, CorX::Circle);
         board.put(1, 1, CorX::Circle);
         board.put(2, 2, CorX::Circle);
-        //todo
-        //assert_eq!(board.winner(), Some(CorX::Circle));
+        assert_eq!(board.winner(), Some(CorX::Circle));
+        let mut board = GomokuBoard::new();
+        board.put(2, 0, CorX::Circle);
+        board.put(1, 1, CorX::Circle);
+        board.put(0, 2, CorX::Circle);
+        assert_eq!(board.winner(), Some(CorX::Circle));
     }
     #[test]
     fn is_puttable() {
